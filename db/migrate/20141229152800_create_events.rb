94d26b62c4215c501c9b0901069e4911e2271485
Ben Murphy <benmurphy@Bens-iMac.local>
2014-12-30 10:30:38 -0500

    events stuff...

---
+++ db/migrate/20141229152800_create_events.rb
@@ -0,0 +1,24 @@
+# Comment migration
+class CreateEvents < ActiveRecord::Migration
+  # Comment migration
+  def change
+    create_table :events do |t|
+      t.references  :user
+      t.string      :type, :limit => 1
+      t.string      :place, :limit => 200
+      t.boolean     :can_others_invite, :default => false
+      t.datetime    :start
+      t.datetime    :end
+      t.boolean     :all_day, :default => false
+      t.string      :name, :limit => 200
+      t.text        :description
+      t.string      :privacy, :limit => 1
+      t.string      :photo
+      t.integer     :location_id, :default => nil
+      t.timestamps
+
+    end
+
+    add_index :events, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141222192948) do
+ActiveRecord::Schema.define(:version => 20141229152900) do
 
   create_table "behaviors", :force => true do |t|
     t.integer  "promotion_id"
