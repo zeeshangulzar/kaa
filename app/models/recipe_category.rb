663e164b48c8bcc5b4f0621a212160ed67706a35
Ben Murphy <benmurphy@Bens-iMac.local>
2015-02-11 12:57:57 -0500

    recipe privacy = public

--- app/models/recipe_category.rb
+++ app/models/recipe_category.rb
@@ -4,7 +4,7 @@ class RecipeCategory < ActiveRecord::Base
   set_primary_key 'id'
  
   attr_privacy_no_path_to_user
-  attr_privacy :id, :recipe_id, :category_type, :category, :any_user
+  attr_privacy :id, :recipe_id, :category_type, :category, :public
  
   MealTypes = ["breakfast","lunch","dinner","snacks"]
   MealTypeNames = {:breakfast => ["breakfast","brunch"],:lunch => ["lunch"],:dinner => ["dinner"],:snacks => ["snacks"]}
