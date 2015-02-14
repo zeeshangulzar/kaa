1a6eb3c42b24b0d569f5fcd38f348def029c0bfd
Ben Murphy <benmurphy@Bens-iMac.local>
2015-02-14 11:16:45 -0500

    photo for chat messages

--- app/uploaders/chat_message_photo_uploader.rb
+++ app/uploaders/chat_message_photo_uploader.rb
@@ -25,7 +25,6 @@ class ChatMessagePhotoUploader < CarrierWave::Uploader::Base
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def original_filename
     @original_filename = "message-#{Time.now.to_i}-#{SecureRandom.hex(16)}.png"
-    return @original_filename
   end
 
   def default_url
