f051b7ee94303e6d07035fb2813400f4f11b6981
Ben Murphy <benmurphy@Bens-iMac.local>
2015-02-26 10:16:24 -0500

    fix attr accessible

--- app/models/success_story.rb
+++ app/models/success_story.rb
@@ -2,6 +2,7 @@ class SuccessStory < ApplicationModel
 
   attr_privacy_path_to_user :user
   attr_privacy :user_id, :user, :title, :summary, :content, :image, :active, :featured, :promotion_id, :submitted1, :submitted2, :submitted3, :submitted4, :submitted_image, :any_user
+  attr_accessible *column_names
 
   belongs_to :promotion
   belongs_to :user
