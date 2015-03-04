7503cf4b615604d4dccd2c8c413adf19bae796bd
Ben Murphy <benmurphy@Bens-iMac.local>
2015-03-04 14:57:15 -0500

    zendesk stuff

--- config/initializers/hes-zendesk.rb
+++ config/initializers/hes-zendesk.rb
@@ -5,3 +5,7 @@ module HesZendesk
   AuthUser = 'admin@hesapps.com'
   AuthPwd = 'H@ppytoH3lp'
 end
+
+# System that manages contact request tickets
+# since zendesk isn't a gem, this has to be called after zendesk lib is loaded, hence why it's here...
+HesContactRequests.ticket = HesZendesk::Ticket
