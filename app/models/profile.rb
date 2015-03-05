eeb0537b6311f732a92d365bf71203059cfeea83
Ben Murphy <benmurphy@Bens-iMac.local>
2015-03-05 09:03:58 -0500

    entity on profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationModel
   # attrs
-  attr_accessible :gender,:first_name,:last_name,:phone,:mobile_phone,:user_id,:updated_at,:created_at, :started_on, :goal_steps, :goal_minutes, :image, :backlog_date, :default_logging_type, :employee_group, :shirt_size, :ethnicity, :age, :is_reward_participant
-  attr_privacy :first_name,:last_name,:phone,:mobile_phone,:user_id,:updated_at,:created_at, :started_on, :goal_steps, :goal_minutes, :backlog_date, :default_logging_type, :employee_group, :shirt_size, :ethnicity, :age, :is_reward_participant, :me
+  attr_accessible :gender,:first_name,:last_name,:phone,:mobile_phone,:user_id,:updated_at,:created_at, :started_on, :goal_steps, :goal_minutes, :image, :backlog_date, :default_logging_type, :employee_group, :shirt_size, :ethnicity, :age, :is_reward_participant, :entity
+  attr_privacy :first_name,:last_name,:phone,:mobile_phone,:user_id,:updated_at,:created_at, :started_on, :goal_steps, :goal_minutes, :backlog_date, :default_logging_type, :employee_group, :shirt_size, :ethnicity, :age, :is_reward_participant, :entity, :me
   attr_privacy :first_name,:last_name,:image,:connections
   attr_privacy :first_name,:last_name,:image,:public_comment
   attr_privacy_path_to_user :user
