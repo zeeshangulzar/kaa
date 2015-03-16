93ab3660703aec0fb20f8056fe32cb1329e912de
Ben Murphy <benmurphy@Bens-iMac.local>
2015-03-15 20:03:11 -0400

    message email better

--- app/jobs/content_email.rb
+++ app/jobs/content_email.rb
@@ -2,6 +2,9 @@ class ContentEmail
   @queue = :default
 
   def self.perform(model, object, emails, user_id, message)
+    # TODO: this is broken because you have to pass ids and
+    # instantiate the objects here because if you pass the objects
+    # they are all converted to hashes and lose their methods and everything sucks
     user = User.find(user_id)
     GoMailer.content_email(model, object, emails, user, message).deliver!
   end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -41,7 +41,6 @@ class Badge < ActiveRecord::Base
     connection.execute("DELETE FROM user_badges WHERE user_id = #{user_id} AND earned_year = #{earned_date.year} AND badge_id IN (#{options[:destroy].join(",")})") unless options[:destroy].empty?
 
     options[:create].each_with_index{|create,index|
-      # TODO: do badge notifications here
       options[:create][index] = [user_id, options[:create][index][0], options[:create][index][1].year, "'#{options[:create][index][1]}'", "'#{now}'", "'#{now}'"].join(',')
       options[:create][index] = "(#{options[:create][index]})"
     }
@@ -175,7 +174,6 @@ class Badge < ActiveRecord::Base
     connection.execute("DELETE FROM user_badges WHERE user_id = #{entry.user.id} AND id IN (#{process[:destroy].join(",")})") unless process[:destroy].empty?
 
     process[:create].each_with_index{|create,index|
-      # TODO: notification
       process[:create][index] = [entry.user.id, process[:create][index][:badge_id], process[:create][index][:earned_date].year, "'#{process[:create][index][:earned_date]}'", "'#{now}'", "'#{now}'"].join(',')
       process[:create][index] = "(#{process[:create][index]})"
     }
@@ -193,7 +191,7 @@ class Badge < ActiveRecord::Base
     Badge.uncached do
       num = post.user.posts.where(:wallable_type => 'Promotion', :wallable_id => post.user.promotion.id, :depth => 0).where("created_at BETWEEN '#{post.created_at.beginning_of_week.to_time.to_s(:db)}' AND '#{post.created_at.end_of_week.to_time.to_s(:db)}'").size
       if num > 2 && post.user.badges_earned.where(:earned_year => post.created_at.year, :badge_id => enthusiast_badge.id).size < 1
-        # TODO: notification and possibly destroy?
+        # TODO: possibly destroy?
         now = post.user.promotion.current_time.to_s(:db)
         connection.execute("INSERT INTO user_badges (user_id, badge_id, earned_year, earned_date, created_at, updated_at) VALUES (#{post.user.id}, #{enthusiast_badge.id}, #{post.created_at.year}, '#{post.created_at.to_date.to_s(:db)}', '#{now}', '#{now}')")
       end
@@ -209,7 +207,6 @@ class Badge < ActiveRecord::Base
       # friendee...
       u = friendship.friendee
       if friendship.friendee && friendship.friendee.friends.size > 9 && friendship.friendee.badges_earned.where(:earned_year => friendship.updated_at.year, :badge_id => sidekick_badge.id).size < 1
-        # TODO: notification
         connection.execute("INSERT INTO user_badges (user_id, badge_id, earned_year, earned_date, created_at, updated_at) VALUES (#{u.id}, #{sidekick_badge.id}, #{friendship.updated_at.year}, '#{friendship.updated_at.to_date.to_s(:db)}', '#{now}', '#{now}')")
       else
         connection.execute("DELETE FROM user_badges WHERE user_id = #{u.id} AND earned_year = #{friendship.updated_at.year} AND badge_id = #{sidekick_badge.id}")
@@ -217,7 +214,6 @@ class Badge < ActiveRecord::Base
       # friender...
       u = friendship.friender
       if friendship.friender && friendship.friender.friends.size > 9 && friendship.friender.badges_earned.where(:earned_year => friendship.updated_at.year, :badge_id => sidekick_badge.id).size < 1
-        # TODO: notification
         connection.execute("INSERT INTO user_badges (user_id, badge_id, earned_year, earned_date, created_at, updated_at) VALUES (#{u.id}, #{sidekick_badge.id}, #{friendship.updated_at.year}, '#{friendship.updated_at.to_date.to_s(:db)}', '#{now}', '#{now}')")
       else
         # remove sidekick badge
@@ -227,7 +223,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_rookie(challenge_received)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     rookie_badge = challenge_received.user.promotion.badges.where(:name => "Rookie").first rescue nil
     return if !rookie_badge
     Badge.uncached do
@@ -241,7 +237,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_mvp(challenge_received)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     mvp_badge = challenge_received.user.promotion.badges.where(:name => "MVP").first rescue nil
     return if !mvp_badge
     Badge.uncached do
@@ -255,7 +251,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_applause(like)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     applause_badge = like.user.promotion.badges.where(:name => "Applause").first rescue nil
     return if !applause_badge
     Badge.uncached do
@@ -269,7 +265,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_high_five(like)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     high_five_badge = like.user.promotion.badges.where(:name => "Applause").first rescue nil
     return if !high_five_badge
     Badge.uncached do
@@ -283,7 +279,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_coach(challenge_sent)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     coach_badge = challenge_sent.user.promotion.badges.where(:name => "Coach").first rescue nil
     return if !coach_badge
     Badge.uncached do
@@ -297,7 +293,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_all_star(success_story)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     all_star_badge = success_story.user.promotion.badges.where(:name => "All Star").first rescue nil
     return if !all_star_badge
     Badge.uncached do
@@ -310,7 +306,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_time_to_shine(success_story)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     time_to_shine_badge = success_story.user.promotion.badges.where(:name => "Time To Shine").first rescue nil
     return if !time_to_shine_badge
     Badge.uncached do
@@ -325,7 +321,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_chef(share)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     chef_badge = share.user.promotion.badges.where(:name => "Chef").first rescue nil
     return if !chef_badge
     return if share.shareable_type != 'Recipe' || !Badge::SOCIAL_MEDIA_TYPES.include?(share.via)
@@ -339,7 +335,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_head_chef(share)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     head_chef_badge = share.user.promotion.badges.where(:name => "Head Chef").first rescue nil
     return if !head_chef_badge
     return if share.shareable_type != 'Recipe' || !Badge::SOCIAL_MEDIA_TYPES.include?(share.via)
@@ -353,7 +349,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_tipster(share)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     tipster_badge = share.user.promotion.badges.where(:name => "Tipster").first rescue nil
     return if !tipster_badge
     return if share.shareable_type != 'Tip' || !Badge::SOCIAL_MEDIA_TYPES.include?(share.via)
@@ -367,7 +363,7 @@ class Badge < ActiveRecord::Base
   end
 
   def self.do_uber_tipster(share)
-    # TODO: notification and possibly destroy?
+    # TODO: possibly destroy?
     uber_tipster_badge = share.user.promotion.badges.where(:name => "Tipster").first rescue nil
     return if !uber_tipster_badge
     return if share.shareable_type != 'Tip' || !Badge::SOCIAL_MEDIA_TYPES.include?(share.via)
