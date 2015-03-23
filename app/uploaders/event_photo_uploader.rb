c2e9e8c70cddd3b50dd179e8a44d4b42a21ce8e1
Ben Murphy <benmurphy@Bens-iMac.local>
2015-03-23 09:46:25 -0400

    refactoring uploaders

--- app/uploaders/event_photo_uploader.rb
+++ app/uploaders/event_photo_uploader.rb
@@ -1,19 +1,7 @@
-# encoding: utf-8
+class EventPhotoUploader < ApplicationUploader
 
-class EventPhotoUploader < CarrierWave::Uploader::Base
-
-  include CarrierWave::RMagick
-
-  storage :hes_cloud
-
-  def store_dir
+  def self.store_dir
     "events/"
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
