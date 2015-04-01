cf64ff4d587d325576437e0992547eccf8863dcc
Ben Murphy <benmurphy@Bens-iMac.local>
2015-04-01 10:05:42 -0400

    emails

--- app/jobs/content_email.rb
+++ app/jobs/content_email.rb
@@ -5,6 +5,7 @@ class ContentEmail
     # TODO: this is broken because you have to pass ids and
     # instantiate the objects here because if you pass the objects
     # they are all converted to hashes and lose their methods and everything sucks
+    ActiveRecord::Base.verify_active_connections!
     user = User.find(user_id)
     GoMailer.content_email(model, object, emails, user, message).deliver!
   end
--- app/jobs/invite_email.rb
+++ app/jobs/invite_email.rb
@@ -2,6 +2,7 @@ class InviteEmail
   @queue = :default
 
   def self.perform(emails, user_id, message = nil)
+    ActiveRecord::Base.verify_active_connections!
     user = User.find(user_id)
     GoMailer.invite_email(emails, user, message).deliver!
   end
