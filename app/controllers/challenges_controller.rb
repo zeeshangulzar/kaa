4deae69d7b9a46d3a58aefcd8fadf17ffe725ab3
Jake Smith <jakes@hesonline.com>
2015-04-15 11:25:00 -0400

    regional challenges limited to specific regions

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -12,7 +12,7 @@ class ChallengesController < ApplicationController
     end
     if params[:type]
       if params[:type] == 'regional_coordinator'
-        challenges = challenges.send('regional').order('visible_from DESC')
+        challenges = challenges.send('regional').where(:location_id => @current_user.top_level_location_id).order('visible_from DESC')
       elsif Challenge::TYPE.stringify_keys.keys.include?(params[:type])
         # ?type=[peer,regional,etc.]
         if params[:type] == 'regional'
