fc91af3b864fb382ccca6013c788c8a2212fb041
Ben Murphy <benmurphy@Bens-iMac.local>
2015-05-01 10:12:47 -0400

    jake needs teams on users search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -201,6 +201,9 @@ class UsersController < ApplicationController
       conditions = ["users.email like ? or profiles.first_name like ? or profiles.last_name like ?",search_string, search_string, search_string]
       p = (@current_user.master? && params[:promotion_id] && Promotion.exists?(params[:promotion_id])) ? Promotion.find(params[:promotion_id]) : @promotion
       users = p.users.find(:all,:include=>:profile,:conditions=>conditions)
+      users.each_with_index{ |user|
+        users[idx].team_id = !user.current_team.nil? ? user.current_team.id : nil
+      }
     else
       limit = !params[:limit].nil? ? params[:limit].to_i : 50
       users = @current_user.unassociated_search(search_string, limit)
