25ad0614cd73f4360e35d5c83c8470070708b287
Drew Papworth <drewp@hesonline.com>
2015-05-13 11:46:33 -0400

    Fixed filtering on aggregate fields.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -66,8 +66,6 @@ class ReportsController < ApplicationController
     @report.filters[:hashes] = hash_filters
     @report.filters[:special] = report_filters_to_special_hash
 
-    # raise @report.get_data.inspect
-
     # get the data
     begin
       data = @report.get_data
