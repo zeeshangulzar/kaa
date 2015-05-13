7b54c7fcd4c7a3e6f393f5e19c6a1cd0965c7a45
Ben Murphy <benmurphy@Bens-iMac.local>
2015-05-13 16:08:27 -0400

    photo uploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,8 @@ class Photo < ApplicationModel
   attr_privacy :name, :caption, :description, :image, :flagged, :flagged_by, :user_id, :photoable_type, :photoable_id, :any_user
   attr_privacy_path_to_user :user
 
+  mount_uploader :image, PhotoImageUploader
+
   scope :not_flagged, where(:flagged => false)
   scope :flagged, where(:flagged => true)
 
