00c58469020ebafc7ec347d3d02c92441c3d6f3d
Ben Murphy <benmurphy@Bens-iMac.local>
2015-05-14 11:19:37 -0400

    user on photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationModel
   belongs_to :user, :foreign_key => "user_id"
   
   attr_accessible *column_names
-  attr_privacy :name, :caption, :description, :image, :flagged, :flagged_by, :user_id, :photoable_type, :photoable_id, :any_user
+  attr_privacy :name, :caption, :description, :image, :flagged, :flagged_by, :user_id, :photoable_type, :photoable_id, :user, :any_user
   attr_privacy_path_to_user :user
 
   mount_uploader :image, PhotoImageUploader
