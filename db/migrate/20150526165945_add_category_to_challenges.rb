f6fc4dddf24a8fa1d5a15d2573940e6920a92932
Ben Murphy <benmurphy@Bens-iMac.local>
2015-06-01 12:17:39 -0400

    challenges migration stuff

--- db/migrate/20150526165945_add_category_to_challenges.rb
+++ db/migrate/20150526165945_add_category_to_challenges.rb
@@ -3,4 +3,18 @@ class AddCategoryToChallenges < ActiveRecord::Migration
     add_column :challenges, :category, :string
     add_column :challenges, :expires_on, :date
   end
+  def up
+    sql = "
+      UPDATE challenges
+      SET challenges.status = 2
+      WHERE challenges.status = 1"
+    self.connection.execute(sql)
+  end
+  def down
+    sql = "
+      UPDATE challenges
+      SET challenges.status = 1
+      WHERE challenges.status = 2"
+    self.connection.execute(sql)
+  end
 end
