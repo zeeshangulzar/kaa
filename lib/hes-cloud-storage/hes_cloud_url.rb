38e95722e14b8b7eadd59d5e3072d5b92050fd69
Ben Murphy <benmurphy@Bens-iMac.local>
2015-06-10 14:18:37 -0400

    fix asset url to always append a number

--- lib/hes-cloud-storage/hes_cloud_url.rb
+++ lib/hes-cloud-storage/hes_cloud_url.rb
@@ -1,7 +1,7 @@
 module HesCloudStorage
 	module HesCloudUrl
 		def cloud_url(with_login_credentials = false)
-      		@cloud_url ||= "#{HesCloudStorage::SUBDOMAIN}#{HesCloudFile.cdn_sequence}.#{HesCloudStorage.configuration[:domain]}"
+      		@cloud_url ||= "#{HesCloudStorage::SUBDOMAIN}#{HesCloudFile.cdn_sequence rescue 1}.#{HesCloudStorage.configuration[:domain]}"
       		"#{HesCloudStorage.configuration[:use_ssl] ? 'https' : 'http'}://#{ "#{HesCloudStorage::USER}:#{HesCloudStorage::PASSWORD}@" if with_login_credentials}#{@cloud_url}"
 		end
 	end
