a14badbdf43148e19277e01f879d03583cfc300b
Ben Murphy <benmurphy@Bens-iMac.local>
2015-06-17 09:29:11 -0400

    null challenge points break things

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -24,7 +24,7 @@ class TeamMember < ApplicationModel
       JOIN (
         SELECT
           user_id,
-          SUM(entries.timed_behavior_points + entries.exercise_points + entries.challenge_points) AS total_points,
+          SUM(COALESCE(entries.timed_behavior_points, 0) + COALESCE(entries.exercise_points, 0) + COALESCE(entries.challenge_points, 0)) AS total_points,
           SUM(entries.exercise_points) AS total_exercise_points,
           SUM(entries.timed_behavior_points) AS total_timed_behavior_points,
           SUM(entries.challenge_points) AS total_challenge_points
