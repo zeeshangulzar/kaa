ae54293bb6aad85cb9a1510e032b4fab8b163d16
Bob Baldwin <bobb@hesonline.com>
2015-09-08 13:33:35 -0400

    Added reorder of behaviors

--- app/controllers/behaviors_controller.rb
+++ app/controllers/behaviors_controller.rb
@@ -1,5 +1,5 @@
 class BehaviorsController < ApplicationController
-  authorize :all, :master
+  authorize :all, :reorder, :master
   authorize :index, :show, :user
   
   def index
@@ -14,9 +14,11 @@ class BehaviorsController < ApplicationController
   end
 
   def create
+    behavior = nil;
     Behavior.transaction do
       behavior = Behavior.create(params[:behavior])
     end
+    return HESResponder(behavior.errors.full_messages, "ERROR") if !behavior.valid?
     return HESResponder(behavior)
   end
 
@@ -26,6 +28,7 @@ class BehaviorsController < ApplicationController
     Behavior.transaction do
       behavior.update_attributes(params[:behavior])
     end
+    return HESResponder(behavior.errors.full_messages, "ERROR") if !behavior.valid?
     return HESResponder(behavior)
   end
 
@@ -36,5 +39,23 @@ class BehaviorsController < ApplicationController
       behavior.destroy
     end
     return HESResponder(behavior)
+  end
+
+  def reorder
+    return HESResponder("Must provide sequence.", "ERROR") if params[:sequence].nil? || !params[:sequence].is_a?(Array)
+    behaviors = @promotion.behaviors
+
+    behavior_ids = behaviors.collect{|behavior|behavior.id}
+    return HESResponder("Behavior ids are mismatched.", "ERROR") if (behavior_ids & params[:sequence]) != behavior_ids
+    sequence = 0
+    params[:sequence].each{ |behavior_id|
+      behavior = Behavior.find(behavior_id)
+      behavior.update_attributes(:sequence => sequence)
+      sequence += 1
+    }
+    Behavior.uncached do
+      behaviors = @promotion.behaviors
+    end
+    return HESResponder(behaviors)
   end
 end
