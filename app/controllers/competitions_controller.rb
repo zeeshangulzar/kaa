15d70b3731de61fe564a9c37ad4ed7da822cce38
Ben Murphy <benmurphy@Bens-iMac.local>
2015-10-01 14:04:03 -0400

    jake broke it

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,5 @@
 class CompetitionsController < ApplicationController
-
+  authorize :index, :show, :create, :update, :destroy, :master
   authorize :members, :coordinator
 
   def index
@@ -7,14 +7,14 @@ class CompetitionsController < ApplicationController
   end
 
   def show
-    competition = @promotion.competitions.find(params[:id])
+    competition = Competition.find(params[:id])
     return HESResponder(competition)
   end
 
   def create
     competition = nil
     Competition.transaction do
-      competition = @promotion.competitions.create(params[:competition])
+      competition = Competition.create(params[:competition])
     end
     return HESResponder("Error saving competition.", "ERROR") if !competition
     return HESResponder(competition.errors.full_messages, "ERROR") if !competition.valid?
@@ -22,7 +22,7 @@ class CompetitionsController < ApplicationController
   end
 
   def update
-    competition = @promotion.competitions.find(params[:id]) rescue nil
+    competition = Competition.find(params[:id]) rescue nil
     return HESResponder("Competition", "NOT_FOUND") if !competition
     Competition.transaction do
       competition.update_attributes(params[:competition])
@@ -32,7 +32,7 @@ class CompetitionsController < ApplicationController
   end
 
   def destroy
-    competition = @promotion.competitions.find(params[:id]) rescue nil
+    competition = Competition.find(params[:id]) rescue nil
     return HESResponder("Competition", "NOT_FOUND") if !competition
     Competition.transaction do
       competition.destroy
