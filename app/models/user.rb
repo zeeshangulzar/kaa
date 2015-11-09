3b9ad31195fd527a6f92536fdb8f75b63f889e89
Ben Murphy <benmurphy@Bens-iMac-2.local>
2015-11-09 14:02:03 -0500

    password for sso

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,6 +79,7 @@ class User < ApplicationModel
 
   # validation
   validates_presence_of :email, :role, :promotion_id, :organization_id, :reseller_id, :password
+  #validates_presence_of :password, :unless => Proc.new { |user| user.promotion.organization.is_sso_enabled }
   validates_uniqueness_of :email, :scope => :promotion_id
 
   belongs_to :promotion
@@ -93,8 +94,8 @@ class User < ApplicationModel
   # hooks
   after_initialize :set_default_values, :if => 'new_record?'
   before_validation :set_parents, :on => :create
+  before_validation :set_sso_password, :on => :create
   before_save :set_top_level_location
-
 
   # includes
   include HESUserMixins
@@ -399,6 +400,12 @@ class User < ApplicationModel
 
   def reset_eligibility
     self.eligibility.update_attributes(:user_id => nil) if !self.eligibility.nil?
+  end
+
+  def set_sso_password
+    if self.promotion.organization.is_sso_enabled && (self.password.nil? || self.password.empty?)
+      self.password = SecureRandom.hex(16) 
+    end
   end
 
 end
