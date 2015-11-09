0b5ae0f0e74cee6b0745d1c0cab3bb44ef2100fb
Bob Baldwin <bobb@hesonline.com>
2015-11-09 12:48:26 -0500

    Organizations need to expose and create whiskey

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,13 @@ class Organization < ApplicationModel
   attr_accessible *column_names
   attr_privacy_no_path_to_user
   attr_privacy :id, :reseller_id, :name, :is_sso_enabled, :is_hes_info_removed, :sso_label, :sso_login_url, :sso_redirect, :public
+  attr_privacy :contact_name, :contact_email, :wskey, :master
   belongs_to :reseller
   has_many :promotions, :dependent => :destroy
+
+  after_initialize :set_default_values, :if => 'new_record?'
+
+  def set_default_values
+    self.wskey ||= SecureRandom.hex(32)
+  end
 end
