395e4d4bcd47bb517c61d5312cfdccf635ad8e57
Bob Baldwin <bobb@hesonline.com>
2016-05-03 16:36:01 -0400

    RM 3836 & 3837: Master dashboard stuff

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,7 @@ class Organization < ApplicationModel
   attr_accessible *column_names
   attr_privacy_no_path_to_user
   attr_privacy :id, :reseller_id, :name, :is_sso_enabled, :is_hes_info_removed, :sso_label, :sso_login_url, :sso_redirect, :public
-  attr_privacy :contact_name, :contact_email, :wskey, :master
+  attr_privacy :contact_name, :contact_email, :wskey, :promotions_count, :master
   belongs_to :reseller
   has_many :promotions, :dependent => :destroy
 
@@ -11,5 +11,10 @@ class Organization < ApplicationModel
 
   def set_default_values
     self.wskey ||= SecureRandom.hex(32)
+  end
+
+
+  def promotions_count
+    self.promotions.size
   end
 end
--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -1,8 +1,16 @@
 class Reseller < ApplicationModel
   attr_accessible *column_names
   attr_privacy_no_path_to_user
-  attr_privacy :name, :created_at, :updated_at, :contact_name, :contact_email, :master
+  attr_privacy :name, :created_at, :updated_at, :contact_name, :contact_email, :organizations_count, :promotions_count, :master
 
   has_many :organizations
   has_many :promotions, :through => :organizations
+
+  def organizations_count 
+    self.organizations.size
+  end
+
+  def promotions_count
+    self.promotions.size
+  end
 end
