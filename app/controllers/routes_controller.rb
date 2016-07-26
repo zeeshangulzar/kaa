bf03852fb4ad73882e4f543ab122711a9a2f4de7
Ben Murphy <benmurphy@Bens-iMac-2.local>
2016-07-26 14:03:29 -0400

    ordered destinations

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -39,13 +39,19 @@ class RoutesController < ApplicationController
   def create
     route = nil
     points = nil
+    ordered_destinations = nil
     if !params[:route][:points].nil?
       points = params[:route].delete(:points) rescue nil
       points = points.to_json # serialized json
     end
+    if !params[:route][:ordered_destinations].nil?
+      ordered_destinations = params[:route].delete(:ordered_destinations) rescue nil
+      ordered_destinations = ordered_destinations.to_json # serialized json
+    end
     Route.transaction do
       route = @SB.new(params[:route]) rescue nil
       route.points = points unless points.nil?
+      route.ordered_destinations = ordered_destinations unless ordered_destinations.nil?
       return HESResponder(route.errors.full_messages, "ERROR") if !route.valid?
       route.save!
     end
@@ -55,13 +61,19 @@ class RoutesController < ApplicationController
     route = Route.find(params[:id]) rescue nil
     return HESResponder("Route", "NOT_FOUND") if route.nil?
     points = nil
+    ordered_destinations = nil
     Route.transaction do
       if !params[:route][:points].nil?
         points = params[:route].delete(:points) rescue nil
         points = points.to_json # serialized json
       end
+      if !params[:route][:ordered_destinations].nil?
+        ordered_destinations = params[:route].delete(:ordered_destinations) rescue nil
+        ordered_destinations = ordered_destinations.to_json # serialized json
+      end
       route.assign_attributes(params[:route])
       route.points = points unless points.nil?
+      route.ordered_destinations = ordered_destinations unless ordered_destinations.nil?
       route.save!
     end
     return HESResponder(route)
