a22f0d0844a57c27a9aa90dd5f88e3bc5997d99d
Ben Murphy <benmurphy@Bens-iMac-2.local>
2016-08-03 14:04:09 -0400

    yay

--- lib/get_uploaded_file/engine.rb
+++ lib/get_uploaded_file/engine.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + "/get_uploaded_file"
 # Engine to initialize HesReactor
 class Engine < ::Rails::Engine
   config.after_initialize do
-  	ActionController::Base.send(:include, GetUploadedFile)
+  	CustomBaseController.send(:include, GetUploadedFile)
   end
 end
