fc114abfd9b66d61f874d8602c0c3b902a43f360
Ben Murphy <benmurphy@Bens-iMac-2.local>
2017-01-13 09:13:19 -0500

    remove name from threshold

--- app/models/point_threshold.rb
+++ app/models/point_threshold.rb
@@ -3,7 +3,7 @@ class PointThreshold < ApplicationModel
   belongs_to :pointable, :polymorphic => true
   belongs_to :promotion
   attr_privacy_no_path_to_user
-  attr_accessible :min, :value, :name, :pointable_type, :pointable_id, :promotion_id
-  attr_privacy :min, :value, :name, :public
+  attr_accessible :min, :value, :pointable_type, :pointable_id, :promotion_id
+  attr_privacy :min, :value, :public
 
 end
